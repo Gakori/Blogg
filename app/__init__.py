487a72a4023d8e361f9acdc44f149dcf2e1f9d27
faith gakori <faithgakori506@gmail.com>
2019-11-29 16:36:35 +0300

    import csrf

--- app/__init__.py
+++ app/__init__.py
@@ -3,26 +3,30 @@ from config import Config
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+csrf=CSRFProtect()
 
 def create_app(config_name):
     app = Flask(__name__)
     
     #creating app configs
     app.config.from_object(Config)
+    app.config['SECRET_KEY'] ='vvasdbbbheh'
     
     #initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    csrf.init_app(app)
     
     #registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
-    
     from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
     
     return app
 
