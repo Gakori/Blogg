f47008816f4d4d39fc40e7b363878ff11352efc8
faith gakori <faithgakori506@gmail.com>
2019-11-29 11:21:09 +0300

    add cinfig options

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    pass
+    
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    pass
+
+    DEBUG = True    
+    
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
