7e34f78be97620d2c3f39ab4dd5e04000ebe0f0c
faith gakori <faithgakori506@gmail.com>
2019-11-30 17:52:09 +0300

    add quotes route

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,16 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
+# from app import app
+from app import db
+from app.main import main
+# from ..request import get_quotes
 
 @main.route('/')
 def index():
     '''
     view root page function that returns index and its data 
     '''
+    # quote = get_quotes()
     
     return render_template('index.html')
