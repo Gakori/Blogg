6b338b91f80c13ff5cbc733eb2a576d277ce2806
faith gakori <faithgakori506@gmail.com>
2019-11-30 17:51:00 +0300

    create get quotes&& add return results

---
+++ app/request.py
@@ -0,0 +1,47 @@
+# import urllib.request,json
+# from .models import Quote
+
+# base_url = None
+
+# def configure_request(app):
+#     global base_url
+    
+#     base_url = app.config['BASE_URL']
+
+# def get_quotes():
+#     '''
+#     function that gets the json response to url request
+#     '''
+
+#     with urllib.request.urlopen(base_url) as url:
+#         data=url.read()
+#         response=json.loads(data)
+        
+#         results=process_quote(response)
+        
+#     return results
+
+# def process_quote(item):
+#     '''
+#     function that processess the movie results
+    
+#     Args:
+#         item:
+#             contains quotes
+#         Returns:
+#             quote_results:quotes objects
+#     '''
+#     results=[]
+    
+#     author=item.get('author')
+#     quote=item.get('quote')
+    
+#     quote_object=Quote(author,quote)
+#     results.append(quote_object)
+    
+#     return results
+        
+        
+        
+        
+   
