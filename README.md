5081be875b0b7059e8cc83c93dfc449334366c2a
faith gakori <faithgakori506@gmail.com>
2019-12-01 19:49:25 +0300

    add update temp

---
+++ README.md
@@ -0,0 +1,61 @@
+## TITLE
+ BLOG
+
+## AUTHOR
+ Built By Faith Gakori
+
+## PROJECT DESCRIPTION
+
+    Blog is an application that enables users to view different types of blogs depending on ones interest.The application also give a chance to the users to post their on blogs,comment on other peoples blog, delete blogs they didnt like and also view different quotes in the same application.
+
+## SCREENSHOTS
+![Screenshot](app/static/images/s1.png)
+![Screenshot](app/static/images/s2.png)
+![Screenshot](app/static/images/s3.png)
+![Screenshot](app/static/images/s4.png)
+![Screenshot](app/static/images/s5.png)
+
+## USER STORIES
+
+* As a user, I would like to view the blog posts on the site
+* As a user, I would like to comment on blog posts
+* As a user, I would like to view the most recent posts
+* As a user, I would like to an email alert when a new post is made by joining a subscription.
+* As a user, I would like to see random quotes on the site
+* As a writer, I would like to sign in to the blog.
+* As a writer, I would also like to create a blog from the application.
+* As a writer, I would like to delete comments that I find insulting or degrading.
+* As a writer, I would like to update or delete blogs I have created.
+
+## Prerequisities
+
+* Flask
+* Pip
+* Python
+
+## SetUp / Installation Requirements
+
+* python3.6
+* pip
+
+## Cloning
+In your terminal:
+* $ git clone https://github.com/gakori/Blogg.git
+* $ cd Blogg
+
+## Running the Application
+To run the application, in your terminal:
+* (virtual)$ chmod a+x start.sh
+* $ ./start.sh
+
+## TECHNOLOGIES USED
+* Python3.6
+* bootstrap
+* flask
+* css
+
+## CONTACT INFORMATION
+ For email reach us through faithgakori506@gmail.com
+
+## LICENCE
+MIT ©2019 Faith Gakori
--- app/main/views.py
+++ app/main/views.py
@@ -4,13 +4,12 @@ from flask_login import login_required,current_user
 from app import db
 from app.main import main
 # from ..request import get_quotes
-from .forms import CommentForm,BlogForm
+from .forms import CommentForm,BlogForm,UpdateProfile
 from ..models import User,Blog,Comment
 import datetime
 from .. import db,photos
 
 @main.route('/')
-# @main.route('/home')
 def index():
     '''
     view root page function that returns index and its data 
@@ -43,6 +42,33 @@ def new_blog():
     title = 'new blog'
     return render_template('new_blog.html',title=title,blog_form=blog_form)
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    user_joined = user.date_joined.strftime('%b %d,%Y')
+    
+    if user is None:
+        abort(404)
+        return render_template('/profile/profile.html',user = user, date = user_joined)
+    
+@main.route('/user/<uname>/update', methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()    
+    if user is None:
+        abort(404)
+        
+        form = UpdateProfile()
+        if form.validate_on_submit():
+            user.bio = form.bio.data
+            
+            db.session.add(user)
+            db.session.commit()
+            
+            return redirect(url_for('.profle',uname=user.username))
+            
+        return render_template('/profile/update.html', form = form)
+        
 @main.route('/user/<uname>/update/pic',methods= ['POST'])
 @login_required
 def update_pic(uname):
@@ -53,3 +79,28 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/blog/<int:id>', methods = ['GET','POST'])
+def blog(id):
+    blog = Blog.get_blog(id)
+    posted_date = blog_posted_strftime('%b, %d, %Y')
+    
+    comment_form = CommentForm()
+    comments = Comment.get_comments(blog)
+    if comment_form.validate_on_submit():
+        comment = comment_form.text.data
+        
+        new_comment = Comment(comment=comment,user=current_user,blog_id = blog)
+        
+        new_comment.save_comment()
+        
+        return render_template('blog.html', comment_form=comment_form, blog=blog, comments=comments, date=posted_date)
+    
+@main.route('/user/<uname>/blogs')
+def user_blogs(uname):
+    user = User.query.filter_by(username=uname).first()
+    blogs = Blog.query.filter_by(user_id = user.id).all()
+    blogs_count = Blog.count_blogs(uname)
+    user_joined = user.date_joined.strftime('%b %d, %Y')
+
+    return render_template("profile/blog.html", user=user,blogs=blogs,blogs_count=blogs_count,date = user_joined)
