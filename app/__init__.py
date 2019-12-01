b5234045b595eb19f8b0f974f0b32c76325bb10b
faith gakori <faithgakori506@gmail.com>
2019-12-01 17:32:21 +0300

    initialize mail message

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -13,9 +15,12 @@ login_manager.loginview = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 csrf=CSRFProtect()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app = Flask(__name__)
+    
     
     #creating app configs
     app.config.from_object(Config)
@@ -26,6 +31,7 @@ def create_app(config_name):
     db.init_app(app)
     csrf.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
     
     #registering the blueprint
     from .main import main as main_blueprint
@@ -34,6 +40,9 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
     
+    #configure uploadset
+    configure_uploads(app,photos)
+    
+    # simple.init_app(app)
+    
     return app
-
-# from app import views
