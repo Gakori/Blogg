1aa7e34c18ec7cd62dd49611c865776eb82b43b2
faith gakori <faithgakori506@gmail.com>
2019-12-03 09:37:42 +0300

    heroku take 2

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='gatfyttr4464erc'
-export MAIL_USERNAME=faithgakori506@gmail.com
-export MAIL_PASSWORD=456789
+export MAIL_USERNAME='faithgakori506@gmail.com'
+export MAIL_PASSWORD='456789'
 
 python3.6 manage.py server
